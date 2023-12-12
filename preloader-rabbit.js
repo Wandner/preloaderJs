420af2361897958efb2dd6aa4633c3b0e9b563ef
Wandner Meirelles <wvmeirelles@gmail.com>
2023-12-11 23:19:19 -0400

    Update preloader-rabbit.js

--- preloader-rabbit.js
+++ preloader-rabbit.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://wandner.github.io/preloaderJs/ripples.svg';
+svgImage.src = 'https://wandner.github.io/preloaderJs/fade-stagger-circles.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
